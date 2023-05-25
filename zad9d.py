0948d8ebe47591d73bf6d7dc4fc97c2b59cdbaef
KarinaYarmosh <91902398+KarinaYarmosh@users.noreply.github.com>
2023-05-25 08:45:19 +0200

    Update zad9d.py

--- zad9d.py
+++ zad9d.py
@@ -1,65 +1,54 @@
+import numpy as np
 import random
 
-# def algorytm_sieci_hopfilda():
-#
+# Set the parameters
+n = 25
+num_iterations = 10
 
-def rand_x():
-    x = random.randint(0, 1)
-    return x
+# Initialize the Boltzmann machine variables
+z =[0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 1, 0, 0,
+    0, 0, 1, 0, 0]
 
-def matrix(xs, n):
-    matrix_c=[[0] * n for i in range(n)]
+# set the initial state of the Boltzmann machine
+x = [random.randint(0, 1) for _ in range(n)]
+
+# Initialize c
+c = np.zeros((n, n), dtype=np.float64)
+
+for i in range(n):
+    for j in range(n):
+        if i != j:
+            c[i, j] = (z[i] - 0.5) * (z[j] - 0.5)
+
+# Initialize weights
+w = np.array(2 * c, dtype=np.float64)
+
+# Initialize theta
+theta = np.zeros(n, dtype=np.float64)
+for i in range(n):
+    theta[i] = sum(c[i])
+
+# Initialize u
+u = np.zeros(n, dtype=np.float64)
+
+# Hopfield network algorithm
+for t in range(1, num_iterations):
+
     for i in range(n):
-        for j in range(n):
-            if i==j:
-                matrix_c[i][j]=0
-            else:
-                matrix_c[i][j]=(xs[i] - 0.5) * (xs[j] - 0.5)
-    return matrix_c
+        u[i] = sum(w[i][j] * x[j] for j in range(n)) - theta[i]
 
-
-# def x_t_plus_1(matrix, n):
-#     for i in range(n):
-#         if u[i] < 0:
-#             x.append(0)
-#         else if:
-#
-#         else:
-
-
-def main():
-    xs = [[0,0,0,0,0],
-          [0,1,1,0,0],
-          [0,0,1,0,0],
-          [0,0,1,0,0],
-          [0,0,1,0,0]]
-    xs_s = [0,0,0,0,0,
-          0,1,1,0,0,
-          0,0,1,0,0,
-          0,0,1,0,0,
-          0,0,1,0,0]
-    n = 25
-    lista_x=[]
     for i in range(n):
-        x = random.randint(0, 1)
-        lista_x.append(x)
-    print("x: ", lista_x)
-    print("xs: ", xs)
-    print("xs: ", xs_s)
-    matrix_c = matrix(xs_s,n)
-    print(matrix_c)
-    #x_t_plus_1()
-    w_i = matrix_c*2
-    print("w_i", w_i)
-    theta = [0 for i in range(n)]
-    for i in range(n):
-        theta[i]=sum(matrix_c[i])
-    print("theta: ",theta)
-    # t - liczba iteracji
-    u = [0 for i in range(n)]
-    for i in range(n):
+        if u[i] > 0:
+            x[i] = 1
+        elif u[i] < 0:
+            x[i] = 0
 
+    print(f"x({t}): ")
+    print(str(np.array(x).reshape(5, 5)))
 
-
-if __name__ == "__main__":
-    main()
+# Print the final state of the Boltzmann machine
+print("Final state:")
+print(str(np.array(x).reshape(5, 5)))
